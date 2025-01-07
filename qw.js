5385e2cef6d882c104328563c111133fa18304b7
SohanHg <sohanhg12@gmail.com>
2025-01-07 09:42:14 +0530

    Add files via upload

---
+++ qw.js
@@ -0,0 +1,3 @@
+// JavaScript logic can be added here for interactivity if needed in the future.
+// Example: Log a message to the console on page load
+console.log("Welcome to Shivakumar's profile!");
